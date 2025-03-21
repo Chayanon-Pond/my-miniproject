9affcb0e0fc0f50b6332fb676eeb6c1f69e68534
Chayanon-Pond <chayanon.kmutnb@gmail.com>
2025-03-21 10:22:56 +0700

    feat(styles): adjust hero-container size and typography

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,11 @@
+const HeroSection = () => {
+  return (
+    <div className="hero-container ">
+      <div>
+        <h1>Movie Survey</h1>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
